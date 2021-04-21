a0bc4a13b0d3afe3c26001630031bcca9589ec23
Zhang Hua <zhanghua.00@qq.com>
2021-04-21 19:42:45 +0800

    加入工具将原版题库转换为GUI版本题库

---
+++ GUI/Tools/PortAnswerDatabase.py
@@ -0,0 +1,24 @@
+import os
+import csv
+import sqlite3
+
+if __name__=="__main__":
+    os.chdir(os.path.split(os.path.realpath(__file__))[0])
+    csvpath=input("请输入原版程序题库（csv文件）的位置：").strip()
+    d=list()
+    with open(file=csvpath,mode="r",encoding="utf-8") as reader:
+        file=csv.reader(reader)
+        for row in file:
+            d.append(row)
+    db=sqlite3.connect("answers.db")
+    try:
+        db.execute("CREATE TABLE 'ALL_ANSWERS' (QUESTION TEXT NOT NULL UNIQUE,ANSWER TEXT NOT NULL)")
+    except sqlite3.OperationalError:
+        print("数据库中似乎已存在 ALL_ANSWERS 表？")
+    for d_ in d:
+        if len(d_)==2:
+            question=str(d_[0])
+            answer="#".join(str(d_[1]).replace("[","").replace("]","").replace("'","").strip().split(", "))
+            db.execute("INSERT OR REPLACE INTO 'ALL_ANSWERS' (QUESTION,ANSWER) VALUES ('%s','%s')" %(question,answer))
+    db.commit()
+    db.close()
